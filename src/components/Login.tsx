57fcd353b6bacca4b57cbf8e533cecc5dc6b11ab
Nasif Shahrier Nafi <nasifshahrierrifat@gmail.com>
2026-09-24 10:53:37 +0600

    Fix github pages base url and oauth redirect

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -8,7 +8,7 @@ export function Login() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: window.location.origin
+        redirectTo: window.location.origin + import.meta.env.BASE_URL
       }
     });
 
